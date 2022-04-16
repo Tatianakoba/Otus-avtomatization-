4e88fd2e271e935477458742ff2670410317dc28
Tatiana <lila-lilu@mail.ru>
2022-04-16 14:32:17 +0300

    Part 1

--- src/Interpreter.py
+++ src/Interpreter.py
@@ -5,16 +5,17 @@
 
 
 '''График'''
+#
+# from pylab import *
+# x = linspace(0, 5, 10)
+# y = x**2
+# print(x)
+# print(y)
+#
+# figure()
+# plot(x, y, 'r')
+# xlabel('x')
+# ylabel('y')
+# title('title')
+# show()
 
-from pylab import *
-x = linspace(0, 5, 10)
-y = x**2
-print(x)
-print(y)
-
-figure()
-plot(x, y, 'r')
-xlabel('x')
-ylabel('y')
-title('title')
-show()
