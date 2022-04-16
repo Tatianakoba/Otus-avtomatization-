47591b6dd7bceec5e936cf7110d5543087c66206
Tatiana <lila-lilu@mail.ru>
2022-04-16 13:45:11 +0300

    Part 1

--- src/Interpreter.py
+++ src/Interpreter.py
@@ -1,4 +1,20 @@
-a = int(input())
-b = int(input())
-print(a+b)
+# a = int(input())
+# b = int(input())
+# print(a+b)
 
+
+
+'''График'''
+
+from pylab import *
+x = linspace(0, 5, 10)
+y = x**2
+print(x)
+print(y)
+
+figure()
+plot(x, y, 'r')
+xlabel('x')
+ylabel('y')
+title('title')
+show()
