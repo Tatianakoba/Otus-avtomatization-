7c40362f5878c72c1a8c62d7b5bb6e9afb889ac0
Tatiana <lila-lilu@mail.ru>
2022-04-28 00:03:00 +0300

    Part 1

--- src/Stepik(лапки).py
+++ src/Stepik(лапки).py
@@ -1,3 +1,13 @@
 username = input('Введите Ваше имя')
 age = input('Введите Ваш возраст')
-print(f"Hello, {username}. You are {age} years old.")
+print(f"Hello, {username}. You are {age} years old."
+      
+'''print("My name is {name}, I'm {age}".format(name = "Natalia", age = 22))
+# My name is Natalia, I'm 22
+
+print("My name is {0}, I'm {1}".format("Maria", 25))
+# My name is Maria, I'm 25
+
+print("My name is {}, I'm {}".format("Juiia", 36))
+# My name is Juiia, I'm 36
+'''
